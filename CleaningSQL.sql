99b3d6925b722b123db15be0059d6e8252a5751e
Dr-JohnWalls <48382586+Dr-JohnWalls@users.noreply.github.com>
2020-06-03 08:28:02 +0100

    Create CleaningSQL.sql

---
+++ CleaningSQL.sql
@@ -0,0 +1,14 @@
+SELECT 
+	-- Concat the strings
+	CONCAT(
+		carrier_code, 
+		' - ', 
+        -- Replicate zeros
+		REPLICATE('0', 9 - LEN(registration_code)), 
+		registration_code, 
+		', ', 
+		airport_code)
+	AS registration_code
+FROM flight_statistics
+-- Filter registers with more than 100 delays
+WHERE delayed > 100
